bc4b996393e50cb6ad3df9f6c74358ae40719f9f
ProsD <alex.prosdocimo@gmail.com>
2026-06-16 12:53:09 +0200

    refactor(core): expose Rich Classes in main lib __init__
    - Add System, Station, Commodity and CommodityMarket to library __init__.py

--- src/pyardent/__init__.py
+++ src/pyardent/__init__.py
@@ -3,7 +3,8 @@ import logging
 from .client import ArdentClient
 from .exceptions import PyArdentError, ResourceNotFoundError, CommodityNotFoundError, SystemNotFoundError, ServiceNotFoundError
 from .types import StationServices, LandingPad
+from .models import System, Station, Commodity, CommodityMarket
 
 logging.getLogger("pyardent").addHandler(logging.NullHandler())
 
-__all__ = ['ArdentClient', 'ResourceNotFoundError', 'CommodityNotFoundError', 'SystemNotFoundError', 'ServiceNotFoundError', 'PyArdentError', "StationServices", "LandingPad"]
+__all__ = ['ArdentClient', 'ResourceNotFoundError', 'CommodityNotFoundError', 'SystemNotFoundError', 'ServiceNotFoundError', 'PyArdentError', "StationServices", "LandingPad", "System", "Station", "Commodity", "CommodityMarket"]
